549500f3d75efcd36b97cd24989038320ba1f262
marcos vinicius <vinicius.dev123@gmail.com>
2025-05-26 23:32:15 -0300

    base do carro

--- carro.py
+++ carro.py
@@ -1,6 +1,36 @@
 import pygame as pg
+import math
 import config
 
 class Carro:
-    def __init__(self):
-        pass
+    def __init__(self, x, y):
+        self.pos = pg.Vector2(x, y)
+        self.angulo = 0
+        self.roda1 = pg.Vector2(x-30, y)
+        self.roda2 = pg.Vector2(x+30, y)
+    
+    def update(self, roda_esquerda, roda_direita):
+        roda1 = pg.Vector2(self.roda1.x, self.roda1.y)
+        roda2 = pg.Vector2(self.roda2.x, self.roda2.y)
+
+        if roda_esquerda:
+            self.angulo += 0.25 
+            self.roda1.x = self.roda2.x - 60 * math.cos(self.angulo)
+            self.roda1.y = self.roda2.y - 60 * math.sin(self.angulo)
+
+            self.roda2.x = self.roda1.x + 60 * math.cos(self.angulo)
+            self.roda2.y = self.roda1.y + 60 * math.sin(self.angulo)
+        
+        if roda_direita:
+            self.angulo -= 0.25
+            self.roda2.x = self.roda1.x + 60 * math.cos(self.angulo)
+            self.roda2.y = self.roda1.y + 60 * math.sin(self.angulo)
+
+            self.roda1.x = self.roda2.x - 60 * math.cos(self.angulo)
+            self.roda1.y = self.roda2.y - 60 * math.sin(self.angulo)
+
+
+    def render(self, tela):
+        pg.draw.line(tela, (100, 120, 100), (self.roda1.x, self.roda1.y), (self.roda2.x, self.roda2.y))
+        pg.draw.circle(tela, (120, 100, 100), (self.roda1.x, self.roda1.y), 5)
+        pg.draw.circle(tela, (120, 100, 100), (self.roda2.x, self.roda2.y), 5)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame as pg
 import config
+from carro import Carro
+
 pg.init()
 
 def main():
@@ -7,14 +9,33 @@ def main():
     clock = pg.time.Clock()
     rodando = True
 
+    carro = Carro(config.TELA_TAMANHO[0]//2, config.TELA_TAMANHO[1]//2)
+    roda_esquerda = roda_direita = False
+
     while rodando:
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 pg.quit()
                 rodando = False
                 return
-        
+            
+            elif e.type==pg.KEYDOWN:
+                if e.key==pg.K_a:
+                    roda_esquerda = True
+
+                elif e.key==pg.K_d:
+                    roda_direita = True
+            elif e.type==pg.KEYUP:
+                if e.key==pg.K_a:
+                    roda_esquerda = False
+
+                elif e.key==pg.K_d:
+                    roda_direita = False
+                
+        carro.update(roda_esquerda, roda_direita)
         tela.fill(config.COR_FUNDO)
+
+        carro.render(tela)
 
         pg.display.update()
         clock.tick(config.FRAMERATE)
